25d11946ac87806362052c02ceee1d5bff06b2d7
Owen <124691791+PiVortex@users.noreply.github.com>
2023-11-09 09:29:05 +0000

    Update BetSlip.jsx
    Change the bet to a ft token transfer

--- Components/BetSlip.jsx
+++ Components/BetSlip.jsx
@@ -2,77 +2,59 @@ const match = props.match;
 const betAmount = props.betAmount;
 const team = props.team;
 
-const yotta = Math.pow(10, 24);
 const terra = Math.pow(10, 12);
 
-const contract = "dev-1689512846991-59182931023942";
-const potentialWinnings = Near.view(contract, "view_potential_winnings", {
+const expandToken = (value, decimals) => {
+  return new Big(value).mul(new Big(10).pow(decimals));
+};
+
+const view_contract = "dev-1689512846991-59182931023942";
+const call_contract = "cusd.fakes.testnet";
+
+const potentialWinnings = Near.view(view_contract, "view_potential_winnings", {
   match_id: match[0],
   team: team,
   bet_amount: betAmount,
 });
 
 const handleClick = () => {
-  Near.call(
-    contract,
-    "make_bet",
-    { match_id: match[0], decision: team },
-    100 * terra,
-    betAmount * yotta
-  );
+  let args = {
+    receiver_id: "dev-1689512846991-59182931023942",
+    amount: expandToken(betAmount, 24).toFixed(),
+    msg: match[0] + " " + team,
+  };
+  Near.call(call_contract, "ft_transfer_call", args, 300 * terra, 1);
 };
 
-const BetSlip = styled.div`
-display: flex;
-flex-direction: column;
-justify-content: space-between;
-align-items: left;
-padding: 2rem;
-`;
-
-const PlaceBetButton = styled.div`
-display: flex;
-justify-content: right;
-grid-column: 1/3;
-`;
-
-const BetGrid = styled.div`
-display: grid;
-grid-template-columns: 50% 50%;
-`;
-
-const Labels = styled.span`
-color: color: #ff1f1f;
-`;
-
 return (
-  <BetSlip>
+  <div>
     <h1>Bet Slip</h1>
-    <BetGrid>
-      <Labels>Match: </Labels>
-      {match[1]} vs {match[3]}
-      <Labels>Date: </Labels>
-      {match[0].slice(-10)}
-      <Labels>Bet Amount: </Labels>
-      {betAmount ? betAmount : "0"}
-      <Labels>Odds:</Labels>
+    <p>
+      Match: {match[1]} vs {match[3]}
+    </p>
+    <p>Date: {match[0].slice(-10)}</p>
+    <p>Bet Amount: {betAmount}</p>
+    <p>
+      Odds:
       {potentialWinnings !== null &&
         potentialWinnings !== undefined &&
         Math.round((potentialWinnings * 100) / betAmount) / 100}
-      Ⓝ<Labels>Potential Winnings:</Labels>
+      Ⓝ
+    </p>
+    <p>
+      Potential Winnings:
       {potentialWinnings !== null &&
         potentialWinnings !== undefined &&
         Math.round(potentialWinnings * 100) / 100}
       Ⓝ
-    </BetGrid>
-    <PlaceBetButton>
-      <button
-        className="btn btn-danger"
-        onClick={() => handleClick()}
-        disabled={potentialWinnings == null || potentialWinnings == undefined}
-      >
-        Place Bet
-      </button>
-    </PlaceBetButton>
-  </BetSlip>
+    </p>
+
+    <button
+      onClick={() => handleClick()}
+      disabled={potentialWinnings == null || potentialWinnings == undefined}
+    >
+      Place Bet
+    </button>
+  </div>
 );
+
